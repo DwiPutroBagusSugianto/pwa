fead9eb492115c4b5d1eb62af85f901d5171ebb9
DwiPutroBagusSugianto <dwiputro060405@gmail.com>
2026-04-25 23:14:15 +0700

    Update Jobs.js

--- client/src/pages/Jobs.js
+++ client/src/pages/Jobs.js
@@ -1,1 +1,23 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
 
+export default function Jobs() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    api.get("/jobs").then(res => setJobs(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Lowongan</h2>
+
+      {jobs.map(job => (
+        <div key={job.id}>
+          <h3>{job.title}</h3>
+          <p>{job.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
