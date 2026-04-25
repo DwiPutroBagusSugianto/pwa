e473ea7acc49a8d7e5164acc82c8b7453c71f129
DwiPutroBagusSugianto <dwiputro060405@gmail.com>
2026-04-25 23:14:33 +0700

    Update Apply.js

--- client/src/pages/Apply.js
+++ client/src/pages/Apply.js
@@ -1,1 +1,24 @@
+import { useState } from "react";
+import api from "../services/api";
 
+export default function Apply() {
+  const [file, setFile] = useState(null);
+
+  const upload = async () => {
+    const formData = new FormData();
+    formData.append("cv", file);
+
+    await api.post("/upload-cv", formData);
+    alert("CV uploaded");
+  };
+
+  return (
+    <div>
+      <h2>Apply</h2>
+
+      <input type="file" onChange={e => setFile(e.target.files[0])} />
+
+      <button onClick={upload}>Upload CV</button>
+    </div>
+  );
+}
