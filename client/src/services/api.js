359889fc3855379037bbf7e2ed499a1e8651ece1
DwiPutroBagusSugianto <dwiputro060405@gmail.com>
2026-04-26 00:10:06 +0700

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,15 +3,4 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://103.217.218.230:5000"
 const db = require("./config/db");
-
-app.post("/auth/register", (req, res) => {
-  const { name, email, password } = req.body;
-
-  const sql = "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, 'user')";
-
-  db.query(sql, [name, email, password], (err, result) => {
-    if (err) return res.status(500).send(err);
-
-    res.send("Register berhasil");
-  });
 });
