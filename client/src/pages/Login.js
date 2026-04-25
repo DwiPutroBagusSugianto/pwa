556cb4b75ef0d0701d6709d3ba31b9b3ab23d39a
DwiPutroBagusSugianto <dwiputro060405@gmail.com>
2026-04-25 23:13:41 +0700

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,1 +1,31 @@
+import { useState } from "react";
+import api from "../services/api";
 
+export default function Login() {
+  const [data, setData] = useState({ email: "", password: "" });
+
+  const login = async () => {
+    const res = await api.post("/auth/login", data);
+
+    localStorage.setItem("token", res.data.token);
+    localStorage.setItem("user", JSON.stringify(res.data.user));
+
+    window.location.href = "/";
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input placeholder="Email"
+        onChange={e => setData({...data, email: e.target.value})}
+      />
+
+      <input type="password" placeholder="Password"
+        onChange={e => setData({...data, password: e.target.value})}
+      />
+
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
