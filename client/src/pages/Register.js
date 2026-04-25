b6d53db7711b6a0e020203c93b965220fdf1a6c7
DwiPutroBagusSugianto <dwiputro060405@gmail.com>
2026-04-25 23:13:57 +0700

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,1 +1,33 @@
+import { useState } from "react";
+import api from "../services/api";
 
+export default function Register() {
+  const [data, setData] = useState({
+    name: "", email: "", password: ""
+  });
+
+  const register = async () => {
+    await api.post("/auth/register", data);
+    alert("Register berhasil");
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <input placeholder="Nama"
+        onChange={e => setData({...data, name: e.target.value})}
+      />
+
+      <input placeholder="Email"
+        onChange={e => setData({...data, email: e.target.value})}
+      />
+
+      <input type="password" placeholder="Password"
+        onChange={e => setData({...data, password: e.target.value})}
+      />
+
+      <button onClick={register}>Register</button>
+    </div>
+  );
+}
