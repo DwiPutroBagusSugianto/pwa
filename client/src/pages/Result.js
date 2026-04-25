6d48a4e8c1190efa12404299863a4e79125ab52a
DwiPutroBagusSugianto <dwiputro060405@gmail.com>
2026-04-25 23:15:07 +0700

    Update Result.js

--- client/src/pages/Result.js
+++ client/src/pages/Result.js
@@ -1,1 +1,22 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
 
+export default function Result() {
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    api.get(`/result/${user.id}`).then(res => setResult(res.data));
+  }, []);
+
+  if (!result) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>Hasil</h2>
+      <p>Nilai: {result.final_score}</p>
+      <p>Status: {result.status}</p>
+    </div>
+  );
+}
